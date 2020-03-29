4bba514d15ec71f062422c1ab4a055cd3e5dc72f
ravczar <ravczar@gmail.com>
2020-03-29 23:42:19 +0200

    The finishing touch.

--- reactiveForm/src/app/app.component.ts
+++ reactiveForm/src/app/app.component.ts
@@ -10,17 +10,28 @@ import { NgForm } from '@angular/forms';
 export class AppComponent implements OnInit {
   @ViewChild('simpleForm') signupForm: NgForm;
   defaultSubValue='advanced';
+  formSubmited:boolean;
   user: User;
 
   constructor(){
-    
+    this.formSubmited = false;
+    this.user = new User('default','advanced','default');
   }
 
-  ngOnInit(){
+  ngOnInit():void{}
 
-  }
-
-  onSubmitForm(){
+  onSubmitForm():void {
     console.log(this.signupForm);
+    this.user = new User(
+      this.signupForm.value.email,
+      this.signupForm.value.subscription,
+      this.signupForm.value.password
+    );
+    this.showNewUserProfileInConsole();
+    this.formSubmited = !this.formSubmited;
+  }
+
+  private showNewUserProfileInConsole(): void{
+    console.log("New user has been added with details: \n" + this.user.expressYourself());
   }
 }
